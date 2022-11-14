ecf7c7d336688675158c9d2440c84a4f7a36d09b
Andrey Nakin <A.Nakin@pulkovo-airport.com>
2022-11-15 01:01:42 +0300

    work

--- pickapic/main.py
+++ pickapic/main.py
@@ -1,6 +1,7 @@
 from .cliapi import parse_args
 from .context import Context
 import caribou
+import os
 
 
 def pickapic():
@@ -9,9 +10,7 @@ def pickapic():
         print("0.1.0")
         return
 
-    migrations_path = '/home/andrey/workspace/pickapic/pickapic/migrations'
-
     # upgrade to most recent version
-    caribou.upgrade(args.config, migrations_path)
+    caribou.upgrade(args.config, os.path.join(os.path.abspath(os.path.dirname(__file__)), 'migrations'))
 
     ctx = Context(args)
