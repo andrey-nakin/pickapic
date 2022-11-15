c49ab6a8f60d5bdfa798f2335228fe30b282f8d8
Andrey Nakin <A.Nakin@pulkovo-airport.com>
2022-11-15 12:59:52 +0300

    work

--- pickapic/cliapi.py
+++ pickapic/cliapi.py
@@ -6,14 +6,15 @@ def get_parser():
     parser.add_argument("--version", "-v", action="store_true", help="Show version")
     parser.add_argument("--config", "-c", help="Path to configuration database file (default: pickapic.db)",
                         default="pickapic.db")
-    parser.add_argument("--search-profile", "-p", help="Search profile to use")
+    parser.add_argument("--profile", "-p", help="Search profile to use")
 
     parser.add_argument("--add-tag", nargs='*', metavar=('profile', 'tag'), help="Add search tag to a profile")
     parser.add_argument("--remove-tag", nargs='*', metavar=('profile', 'tag'), help="Remove search tag from a profile")
     parser.add_argument("--list-tags", nargs=1, metavar='profile', help="List profile search tags")
 
     parser.add_argument("--add-stop-tag", nargs='*', metavar=('profile', 'tag'), help="Add tag to a profile black list")
-    parser.add_argument("--remove-stop-tag", nargs='*', metavar=('profile', 'tag'), help="Remove tag from a profile black list")
+    parser.add_argument("--remove-stop-tag", nargs='*', metavar=('profile', 'tag'),
+                        help="Remove tag from a profile black list")
     parser.add_argument("--list-stop-tags", nargs=1, metavar='profile', help="List profile black list tags")
 
     parser.add_argument("--add-global-tag", nargs='?', metavar='tag', help="Add tag to global list")
--- pickapic/context.py
+++ pickapic/context.py
@@ -2,8 +2,31 @@ import sqlite3
 
 
 class Context:
+
     def __init__(self, args):
         self.args = args
+        self.cached_profile_id = None
 
     def connection(self):
         return sqlite3.connect(self.args.config)
+
+    def profile_id(self):
+        if self.cached_profile_id is None:
+            assert self.args.profile is not None
+
+            conn = self.connection()
+            cur = conn.cursor()
+            cur.execute("SELECT id FROM profile WHERE name = ?", (self.args.profile,))
+            conn.commit()
+
+            row = cur.fetchone()
+            if not row:
+                cur.execute("INSERT INTO profile (name) VALUES (?)", (self.args.profile,))
+                conn.commit()
+                self.cached_profile_id = cur.lastrowid
+            else:
+                self.cached_profile_id = row[0]
+
+            conn.close()
+
+        return self.cached_profile_id
