abaf1e9cd90ececf929bc49570714164d66321e5
Andrey Nakin <A.Nakin@pulkovo-airport.com>
2022-11-19 23:02:49 +0300

    work

--- pickapic/process.py
+++ pickapic/process.py
@@ -1,4 +1,6 @@
 from .driver import flickr
+from PIL import Image
+import math
 
 
 def process(context, num_of_images):
@@ -13,4 +15,24 @@ def process(context, num_of_images):
 
 
 def _process_image(context, image):
+    original = Image.open(image.filename)
+    width, height = original.size
+    target_width, target_height = context.min_dimensions()
+    if width >= target_width and height >= target_height:
+        aspect_ratio = width / height
+        target_aspect_ratio = target_width / target_height
+
+        if aspect_ratio >= target_aspect_ratio:
+            original.thumbnail((math.floor(aspect_ratio * target_height), target_height),
+                               Image.Resampling.LANCZOS)
+        else:
+            original.thumbnail((target_width, math.floor(target_width / aspect_ratio)),
+                               Image.Resampling.LANCZOS)
+
+        width, height = original.size
+        min_x = math.floor((width - target_width) / 2)
+        min_y = math.floor((height - target_height) / 2)
+        cropped = original.crop((min_x, min_y, min_x + target_width, min_y + target_height))
+        cropped.save("cropped.jpg")
+
     print(image)
