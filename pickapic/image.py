afd34911ec3de98f20abdeaa2edf6991fea2d3f7
Andrey Nakin <A.Nakin@pulkovo-airport.com>
2022-11-25 22:29:19 +0300

    work

--- pickapic/image.py
+++ pickapic/image.py
@@ -1,6 +1,7 @@
 import math
 from PIL import Image
 from PIL import ImageFile
+import piexif
 
 
 def resize_and_crop(srcfile, destfile, target_dimensions):
@@ -24,3 +25,24 @@ def resize_and_crop(srcfile, destfile, target_dimensions):
     min_y = math.floor((height - target_height) / 2)
     cropped = original.crop((min_x, min_y, min_x + target_width, min_y + target_height))
     cropped.save(destfile)
+
+
+def set_exif_info(image_file_name, image_description=None, copyright=None, artist=None):
+    im = Image.open(image_file_name)
+    try:
+        exif_dict = piexif.load(im.info["exif"])
+    except KeyError:
+        exif_dict = dict({'0th': {}})
+
+    if image_description:
+        exif_dict["0th"][piexif.ImageIFD.ImageDescription] = _encode_exif_tag(image_description)
+    if copyright:
+        exif_dict["0th"][piexif.ImageIFD.Copyright] = _encode_exif_tag(copyright)
+    if artist:
+        exif_dict["0th"][piexif.ImageIFD.Artist] = _encode_exif_tag(artist)
+    exif_bytes = piexif.dump(exif_dict)
+    im.save(image_file_name, "jpeg", exif=exif_bytes)
+
+
+def _encode_exif_tag(s):
+    return s.encode(encoding='UTF-8', errors='strict') if s else ''
--- pickapic/process.py
+++ pickapic/process.py
@@ -1,6 +1,9 @@
-from .flickr.process import flickr_process
 import os
-from .image import resize_and_crop
+
+from .flickr.process import flickr_process
+from .image import resize_and_crop, set_exif_info
+
+SEPARATOR = ' | '
 
 
 def process(context, num_of_images):
@@ -15,7 +18,8 @@ def process(context, num_of_images):
 
 
 def _process_image(context, image):
-    resize_and_crop(image.filename, os.path.join(context.args.dest_dir, image.destname), context.min_dimensions())
+    destname = os.path.join(context.args.dest_dir, image.destname)
+    resize_and_crop(image.filename, destname, context.min_dimensions())
     os.unlink(image.filename)
 
     print(image.title)
@@ -24,3 +28,23 @@ def _process_image(context, image):
     print(image.author_desc.page_url)
     print(image.license_desc.name)
     print(image.license_desc.page_url)
+
+    image_info = None
+    if image.title:
+        image_info = image.title
+        if image.image_page_url:
+            image_info = image_info + SEPARATOR + image.image_page_url
+
+    author_info = None
+    if image.author_desc and image.author_desc.name:
+        author_info = image.author_desc.name
+        if image.author_desc.page_url:
+            author_info = author_info + SEPARATOR + image.author_desc.page_url
+
+    license_info = None
+    if image.license_desc and image.license_desc.name:
+        license_info = image.license_desc.name
+        if image.license_desc.page_url:
+            license_info = license_info + SEPARATOR + image.license_desc.page_url
+
+    set_exif_info(destname, image_description=image_info, artist=author_info, copyright=license_info)
